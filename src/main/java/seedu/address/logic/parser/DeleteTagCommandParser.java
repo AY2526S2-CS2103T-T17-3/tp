35ddfe61410d0abadba94c34329265f3140a5f75
yytelliot <yytelliot@gmail.com>
2026-03-15 17:29:08 +0800

    Add parsers for Tag Commands

---
+++ src/main/java/seedu/address/logic/parser/DeleteTagCommandParser.java
@@ -0,0 +1,33 @@
+package seedu.address.logic.parser;
+
+import java.util.Set;
+
+import seedu.address.commons.core.index.Index;
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import seedu.address.logic.commands.DeleteTagCommand;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.tag.Tag;
+
+/**
+ * Parses input arguments and creates a new DeleteTagCommand object.
+ */
+public class DeleteTagCommandParser implements Parser<DeleteTagCommand> {
+
+    @Override
+    public DeleteTagCommand parse(String args) throws ParseException {
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_TAG);
+
+        if (argMultimap.getPreamble().isEmpty() || argMultimap.getAllValues(PREFIX_TAG).isEmpty()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteTagCommand.MESSAGE_USAGE));
+        }
+
+        try {
+            Index index = ParserUtil.parseIndex(argMultimap.getPreamble());
+            Set<Tag> tags = ParserUtil.parseTags(argMultimap.getAllValues(PREFIX_TAG));
+            return new DeleteTagCommand(index, tags);
+        } catch (ParseException pe) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteTagCommand.MESSAGE_USAGE), pe);
+        }
+    }
+}
